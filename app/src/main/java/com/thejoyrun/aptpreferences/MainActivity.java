c7daa98c289896cac226c548875c4df17ad012b8
Wiki <taoweiji2008@qq.com>
2016-08-06 17:58:52 +0800

    优化生成代码

--- app/src/main/java/com/thejoyrun/aptpreferences/MainActivity.java
+++ app/src/main/java/com/thejoyrun/aptpreferences/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SettingsPreferences settingsPreference = SettingsPreferences.get("name");
+        SettingsPreferences settingsPreference = SettingsPreferences.get();
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("last value firstUse: ").append(settingsPreference.getUseLanguage()).append('\n');
         settingsPreference.setUseLanguage(new Date().toString());
